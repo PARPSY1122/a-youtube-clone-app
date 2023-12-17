ce1c3f642c93b08861806dd9b6de508a2bd176e6
PARPSY1122 <119403673+PARPSY1122@users.noreply.github.com>
2023-12-17 13:02:46 +0530

    Update main.tf

--- servers/monitoring-servers/main.tf
+++ servers/monitoring-servers/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "web" {
     instance_type = "t2.medium"
     key_name = "ajay" #changeyourkeypair
     vpc_security_group_ids = [ aws_security_group.monitoring-sg.id ]
-    user_data = templatefile("./script.sh", {})
+    user_data = templatefile("./scripts.sh", {})
 
     tags = {
         name = "monitoring-servers"
